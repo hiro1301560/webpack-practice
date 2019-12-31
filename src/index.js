0154a5623bdf9a2bd70e2fd92dde015cd3fd681d
hiro1301560 <junjapo.spirit@gmail.com>
2019-12-31 11:53:36 +0900

    [Add]baabel-loader and react development

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import _ from 'lodash'
-import './style.css';
-import './style.scss';
-import logo from './e-town0010-s1024_town0010_11-480x302.jpg'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function component() {
-  const element = document.createElement('div');
-  const array = ['Hello', 'webpack', '!']
-  element.innerHTML = _.join(array, ' ')
-  return element;
-}
-
-document.body.appendChild(component());
-document.body.classList.add('haikei');
-const image = new Image()
-image.src = logo
-document.body.appendChild(image);
+ReactDOM.render(
+  <div>Hello, React!</div>,
+  document.getElementById('root')
+)
